ff7a0aba964df0114ef7f78b4d01ceb171d3bfcd
ZacRoss <37720721+ZacRoss@users.noreply.github.com>
2018-04-22 22:59:02 +1000

    Table changes and additions
    Also added a command document, which will hopefully end up having everything someone might need.

--- databaseTables/storeDatabaseTables.sql
+++ databaseTables/storeDatabaseTables.sql
@@ -7,31 +7,62 @@
                 Also did some formatting work to make it easier to read.
     15/04/2018 - Changed Customer table name to Users to align with other code,
                  alongside the first column name.
+    22/04/2018 - Added new table (ServiceDescriptions).
+                Changed Items table to services table to 
+                better reflect the clients needs.
+                Put in a number assignment. 
+                Hopefully add booking functionality.
 */
 
-CREATE TABLE Items (
-    ItemID              INT             NOT NULL    AUTO_INCREMENT,
-    ItemName            VARCHAR(50)     NOT NULL, 
-    ItemPrice           DECIMAL(15,2)   NOT NULL,
-    ItemQuantity        INT             NOT NULL,
-    PRIMARY KEY (ItemID)
+CREATE TABLE Services (
+    ServiceID                   INT             NOT NULL    AUTO_INCREMENT,
+    ServiceName                 VARCHAR(50)     NOT NULL, 
+    ServicePrice                DECIMAL(15,2)   NOT NULL,
+    ServiceType                 INT             NOT NULL,
+    ServiceTime                 INT,
+    PRIMARY KEY (ServiceID)
 );
+/* Service types (massages, eyelash extensions and others will be assigned a number) 
+    This will help in displaying data depending on the users viewing selection */
+/* For now services will be assigned as follows:
+Massage             = 1
+Spa                 = 2
+Eyelash extensions  = 3
+*/
+/* Service time will be stored as an integer to simplify the storing process.
+    It can be stored as NULL due to not all services having a time assignment,
+    though I do assume that they will eventually have one. */
+
+CREATE TABLE ServiceDescription
+(
+    ServiceDescriptionID        INT             NOT NULL    AUTO_INCREMENT,
+    ServiceID                   INT             NOT NULL,
+    ServiceDescription          TEXT,
+    PRIMARY KEY (ServiceDescriptionID), 
+    FOREIGN KEY (ServiceID) REFERENCES Services(ServiceID)
+);
+/* 
+    Due to the nature of the services potentially being divied 
+    between time allocations, we may wither need loops to ensure both entries will
+    have the same desctiption if ever the situation arose where one was
+    deleted.
+*/
 
 CREATE TABLE Discount 
 (
-    DiscountID          INT             NOT NULL    AUTO_INCREMENT,
-    DiscountCode        VARCHAR(50)     NOT NULL,
-    DiscountPercentage  INT             NOT NULL,
+    DiscountID                  INT             NOT NULL    AUTO_INCREMENT,
+    DiscountCode                VARCHAR(50)     NOT NULL,
+    DiscountPercentage          INT             NOT NULL,
     PRIMARY KEY (DiscountID)
 );
 
 CREATE TABLE Staff
 (
-    StaffID             INT             NOT NULL    AUTO_INCREMENT,
-    FirstName           VARCHAR(50)     NOT NULL,
-    LastName            VARCHAR(50)     NOT NULL,
-    Username            VARCHAR(50)     NOT NULL,
-    Passphrase          VARCHAR(50)     NOT NULL,
+    StaffID                     INT             NOT NULL    AUTO_INCREMENT,
+    FirstName                   VARCHAR(50)     NOT NULL,
+    LastName                    VARCHAR(50)     NOT NULL,
+    Username                    VARCHAR(50)     NOT NULL,
+    Passphrase                  VARCHAR(50)     NOT NULL,
     PRIMARY KEY (StaffID)
 );
 
@@ -39,37 +70,37 @@ CREATE TABLE Staff
 CREATE TABLE Users
 (
     /* First field name changed */
-    UserID              INT             NOT NULL    AUTO_INCREMENT,
-    FirstName           VARCHAR(50)     NOT NULL,
-    LastName            VARCHAR(50)     NOT NULL,
-    EmailAddress        VARCHAR(100)    NOT NULL,
-    Username            VARCHAR(50)     NOT NULL,
-    Passphrase          VARCHAR(50)     NOT NULL,
-    PRIMARY KEY (CustomerID)
+    UserID                      INT             NOT NULL    AUTO_INCREMENT,
+    FirstName                   VARCHAR(50)     NOT NULL,
+    LastName                    VARCHAR(50)     NOT NULL,
+    EmailAddress                VARCHAR(100)    NOT NULL,
+    Username                    VARCHAR(50)     NOT NULL,
+    Passphrase                  VARCHAR(50)     NOT NULL,
+    PRIMARY KEY (UserID)
 );
 
 /* Need a one to many relation table in addition */
 CREATE TABLE Orders
 (
-    OrderID             INT             NOT NULL    AUTO_INCREMENT,
-    CustomerID          INT             NOT NULL,
+    OrderID                     INT             NOT NULL    AUTO_INCREMENT,
+    UserID                      INT             NOT NULL,
     /* ItemID  INT NOT NULL, */
     /* will need to be able to implement multiple item ID's, or else
         people will only be able to order one thing at a time */
-    OrderDate           VARCHAR(50)     NOT NULL,
-    TotalOrderTab       DECIMAL(15,2)   NOT NULL,
+    OrderDate                   VARCHAR(50)     NOT NULL,
+    TotalOrderTab               DECIMAL(15,2)   NOT NULL,
     PRIMARY KEY (OrderID),
-    FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
     /* FOREIGN KEY (ItemID) REFERENCES Items(ItemID) */
 );
     
-CREATE TABLE ItemsInOrder
+CREATE TABLE ServicesInOrder
 (
-    OrderID             INT             NOT NULL,
-    ItemID              INT             NOT NULL,
-    FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
-    FOREIGN KEY (Orders) REFERENCES Orders(OrderID)
-)
+    OrderID                     INT             NOT NULL,
+    ServiceID                   INT             NOT NULL,
+    FOREIGN KEY (ServiceID) REFERENCES Services(ServiceID),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
 
 /* Customer will make an order and  */
 
@@ -82,4 +113,14 @@ CREATE TABLE ItemsInOrder
         number assigned to it (the one in the same session), and then
         entered into the database.
         This is repeated until there are no items left in the cart.
+*/
+
+/* Vestigial tables for reference and backtracking
+CREATE TABLE Items (
+    ItemID              INT             NOT NULL    AUTO_INCREMENT,
+    ItemName            VARCHAR(50)     NOT NULL, 
+    ItemPrice           DECIMAL(15,2)   NOT NULL,
+    ItemQuantity        INT             NOT NULL,
+    PRIMARY KEY (ItemID)
+);
 */

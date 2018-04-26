aecc0b68a86777ef24be7ddd8771f4c68b894ec4
merbache <38714691+merbache@users.noreply.github.com>
2018-04-26 18:32:24 +1000

    Add files via upload

---
+++ mellowMeOut_Website/mellowMeOut_Website/footer.php
@@ -0,0 +1,52 @@
+<footer>
+<!-- Social Media -->
+        <div class="footer1">
+        <h4>Follow Us</h4>
+          
+            <ul>
+                <li><a href="a">Facebook</a></li>
+                <li><a href="a">Instagram</a></li>
+                <li><a href="a">Twitter</a></li>
+            </ul>
+        </div>
+        
+        <!-- Help area -->
+        <div class="footer2">
+        <h4>Help</h4>
+            
+             <ul>
+                 <li><a href="a">Contact Us</a></li>
+                 <li><a href="a">FAQ</a></li>
+                 <li><a href="a">Policy</a></li>
+                
+            </ul>
+        </div>
+
+        <div class="footer4">
+        <h4>Store Hours</h4>
+            
+             <ul>
+                 <li><b>Mon:</b> 9am - 5pm</li>
+                 <li><b>Tue:</b> 9am - 5pm</li>
+                 <li><b>Wed:</b> 9am - 5pm</li>   
+                 <li><b>Thu:</b> 9am - 5pm</li>
+                 <li><b>Fri:</b> 9am - 5pm</li>
+                 <li><b>Sat:</b> Closed</li>
+                 <li><b>Sun:</b> Closed</li>
+                
+            </ul>
+        </div>
+        <!-- Newsletter area -->
+        <div class="footer3">
+            <h4>Newsletter</h4>
+            <p>Signup to our newsletter for the latest news</p>
+            <form action="index.html" method="post">
+                <input type="email" name="email"  placeholder="Email address" autofocus maxlength="32" size="14">
+                <button type="submit">SUBSCRIBE</button>
+            </form>
+  
+        </div>
+        <div class="footerCopyright">
+            <p>© 2018 Mellow Me Out</p>
+        </div>
+</footer> 

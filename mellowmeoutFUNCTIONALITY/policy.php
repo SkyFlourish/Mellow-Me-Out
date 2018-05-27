4435d890a0287d55f98911c4bded99b071b25a2c
Con-Grant <37649444+Con-Grant@users.noreply.github.com>
2018-05-27 22:48:43 +1000

    Added body text for privacy policy

--- mellowmeoutFUNCTIONALITY/policy.php
+++ mellowmeoutFUNCTIONALITY/policy.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,45 +9,100 @@
     
     <meta name="description" content="Relaxing Spas. Book now today!">
     
-    <link rel="stylesheet" type="text/css" href="policy.css"> <!-- Link to external CSS file -->
+    <link rel="stylesheet" type="text/css" href="about-us.css"> <!-- Link to external CSS file -->
     <script type="text/javascript" src="slideshow.js"></script>
 </head>
     
 <body onload="ChangeDots">
     <div class="wrapper">
-        <!-- Header -->
         <?php include('header.php')?>
-        
-    
-    <!-- Navigation bar -->
         <?php include('nav.php')?>
     
-
     <!-- MAIN CONTENT -->
     <div class= "content"> 
-        <h2>Privacy Policy</h2>
-        <p> Effective Date:  May 1, 2018</p>
-        <p>This privacy notice discloses the privacy practices for MellowMeOut and our website; http://www. [Your Website URL Here]. This privacy notice applies solely to information collected by this website, except where stated otherwise. It will notify you of the following:</p>
-        <ul>
-        <li> What information we collect</li>
-            <li> Who it is shared to</li>
-            <li> How it is secured</li>
-        </ul>
-        
+     <div class= "textbody"><br> <br>
+ <h1>Privacy Policy</h1>
+	
+Mellow Me Out is devoted to delivering quality services and amenities to you and this privacy policy outlines our continuing obligations to you in how we manage, store and utilize your Personal Information.
+Personal information is information or judgement that is documented in any form, whether true or false, about an individual whose identity is apart or can judiciously be determined.
+Mellow Me Out is bound by the Australian Privacy Principles (APPs) controlled in the Privacy Act 1988 (Cth) (the Privacy Act). The NPPs administer the means in which we use, collect, store, disclose and dispose of your Personal Information.
+Information in detailed form about the Act is available and can be attained from the website of The Office of the Australian Information Commissioner at www.aoic.gov.au
+<h3>What is Personal Information and why do we collect it?</h3>
+Personal Information is information or an opinion that identifies an individual. Examples of Personal Information collected from us include: first names, last names, home addresses, email addresses, phone numbers, IP addresses, cookie information, location data and details of age.
+This Personal Information is obtained through various methods including:
+•	Submitting an enquiry.
+•	If customer’s contact Mellow Me out, Mellow Me Out may keep record of that correspondence.
+•	Details of customer’s visits to Mellow Me Out including but not limited to, traffic data, location data, cookies, location data, whether this is required for billing purposes or the resources the customer accesses.
+•	Subscribing to a newsletter.
+•	Making a booking.
+•	Submitting some type of forms.
+•	Utilizing our social media platforms.
+•	Utilizing Google Analytics for the Mellow Me Out website.
+Mellow Me Out collects Personal Information for the principal purpose of delivering services and amenities to you, providing information to our clients and for marketing purposes. We may also use your Personal Information for secondary purposes closely related to the primary purpose, in conditions where you would judiciously require such use or admission. By contacting Mellow Me Out through in writing you can unsubscribe from our mailing/marketing lists at any time.
+Where possible and appropriate, we will collect Personal Information, explaining to you as to why are collecting the information and how we plan to utilize it.
+By using this website, you agree to the managing of data about you by Google explained in Google’s Privacy Policy. https://policies.google.com/privacy.
+You can opt out by going to Ad Settings https://adssettings.google.com/authenticated?hl=en
+<h3>Sensitive Information</h3>
+In the Privacy Act, sensitive information includes information or details about an individual’s political opinions, religious or philosophical beliefs, criminal record or health record, membership of a political association, member of a trade or other professional body.
+Sensitive information will be utilized by us only:
+•	With your consent; or where required or authorised by law.
+<h3>IP Addresses and Cookies</h3>
+Mellow Me Out may collect information about customer’s interactions with the website including, Customer’s IP addresses, browser types, operating systems, location data, cookie information. This is all statistical data and does not identify any individual utilizing the Mellow Me Out website.
+<h3>Third Parties</h3>
+Personal Information will be collected only from you where it is sensible and feasible to do so. In some conditions we may be supplied with Personal Information from and by third parties. We will take reasonable steps in such a case to guarantee you are made aware of information given to us by third parties.
+<h3>Social Media</h3>
+Individuals and businesses who wish to engage with Mellow Me Out via all social media channels, which include but is not limited to, Facebook, Twitter, Instagram and Youtube must be aware of the following:
+1.	Any information you post on social media sites is potentially available to anyone engaging with Mellow Me Out via the same social media.
+2.	Social media platforms have specific privacy policies which all users need to be aware of and adhere to when using these channels for personal, private or business use. Mellow Me Out is not liable for the security or privacy of users who utilize or contact our social media platforms.
+3.	Your social media profile contact details may be used by Mellow Me Out to contact you for feedback.
+4.	Mellow Me Out only collects information that you voluntarily post via responses, posts, comments, tweets and polls.
+<h3>Disclosure of Personal Information</h3>
+In a number of circumstances Personal Information may be disclosed including the following:
+•	If Mellow Me Out sell or buy any business assets, in which case Mellow Me Out may disclose customer’s personal data.
+•	Where required or authorised by law.
+Security of Personal Information
+Personal Information is kept in a way that sensibly guards it from misappropriation, loss, disclosure and unauthorized access or modification from any group or individual.
+We will in a secure manager retain any information provided to us regarding a transaction for a period of seven years after which we may remove.
+Most Personal Information is or will be stored and only kept by us for a minimum of 7 years.
+When your Personal Information is no longer required for the reason it was attained, Mellow Me Out will take sensible measures to permanently de-identify your Personal Information or destroy it.
+<h3>Access to your Personal Information</h3>
+You may request personal information we have collected about you. You may request to correct and/or update it. If you wish to access your Personal Information, please contact us in writing.
+Mellow Me Out will not charge any fee for your access request.
 
-        </div>
-        
-      
+We will require personal identification, such as a drivers license from you before releasing Personal Information to protect your identity.
+<h3>Maintaining the Quality of your Personal Information</h3>
+We take sensible practices to make sure that your Personal Information is correct, complete and up-to-date. If you find the information we have is inaccurate or not up to date, please advise us as soon as possible so that we can ensure we can continue to provide quality services to you and have updated information in our records.
+<h3>Data Quality and Security</h3>
+Mellow Me Out takes reasonable precautions to protect information from unauthorised use.
+We advise users to follow security practices when using Mellow Me Out services. 
+Basic practices recommend:
+•	Ensure the website is HTTPS and is authentic using Secure Socket Layer (SSL).
+•	Use reputable anti-virus software and keep software up to date.
+•	Install security updates for all software on computer.
+<h3>Policy Updates</h3>
+The privacy policy is available from our website and may change from time to time.
+<h3>Privacy Policy Complaints and Enquiries</h3>
+For any complaints or queries about our Privacy Policy, please contact us at:
+Address: First Floor, 171 Boronia Rd, Boronia VIC 3155 
+Email: snezana.mitrovic@hotmail.com
+Phone: 0490 931 319
+
+
+</div>
+
+
+
+    </div>
     <!-- MAIN CONTENT END -->
     <!-- FOOTER CONTENT -->
-       <?php include('footer.php')?>  
+<?php include('footer.php')?>
              <style>
             /* dynamic styling of footer across all pages */
             <?php include 'footer.css'; ?>
         </style>
 </div>
     
-  
+    
    
 
 </body>

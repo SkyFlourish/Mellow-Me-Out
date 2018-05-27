e95f16adcf5251e38c264fdcb63a91384669e0dc
AJ NIKOLAOU <anikolao@deakin.edu.au>
2018-05-28 00:13:55 +1000

    Javascript Valadation 2.0
    Accidently reverted changes on the policy page. Should be back to normal

--- mellowmeoutFUNCTIONALITY/footer.php
+++ mellowmeoutFUNCTIONALITY/footer.php
@@ -1,3 +1,46 @@
+<script tupe ="text/javascript">
+
+function validateLogin()
+{
+  var x = document.forms["login"]["uid"].value;
+  var y = document.forms["login"]["pwd"].value;
+
+  if (x == "")
+  {
+      alert("A username is required to login.");
+      return false;
+  }
+  if(y == "")
+  {
+    alert("A password is required to login");
+    return false;
+  }
+}
+
+// Validation for the Newsletter
+function validateNewsletter()
+{
+  // Gets the input
+  var x = document.forms["newsLetter"]["email"].value;
+  //checks if empty
+  if (x == "")
+  {
+    //alerts and returns false if true
+    alert("An email needs to be supplied");
+    return false;
+  }
+  else if (!x.includes("@") || !x.includes("."))
+  {
+    alert("That email is not in the proper format please try again.")
+    return false;
+  }
+
+}
+
+
+</script>
+
+
 <footer>
 <!-- Social Media -->
 
@@ -5,81 +48,85 @@
 
         <div class="footer1">
         <h4>Follow Us</h4>
-          
+
             <ul>
                 <li><a href="a">Facebook</a></li>
                 <li><a href="a">Instagram</a></li>
                 <li><a href="a">Twitter</a></li>
             </ul>
         </div>
-        
+
         <!-- Help area -->
         <div class="footer2">
         <h4>Help</h4>
-            
+
              <ul>
                  <li><a href="contact-us.php">Contact Us</a></li>
                  <li><a href="faq.php">FAQ</a></li>
                  <li><a href="policy.php">Privacy Policy</a></li>
-                
+
             </ul>
         </div>
 
         <div class="footer4">
         <h4>Store Hours</h4>
-            
+
              <ul>
                  <li><b>Mon:</b> 9am - 5pm</li>
                  <li><b>Tue:</b> 9am - 5pm</li>
-                 <li><b>Wed:</b> 9am - 5pm</li>   
+                 <li><b>Wed:</b> 9am - 5pm</li>
                  <li><b>Thu:</b> 9am - 5pm</li>
                  <li><b>Fri:</b> 9am - 5pm</li>
                  <li><b>Sat:</b> Closed</li>
                  <li><b>Sun:</b> Closed</li>
-                
+
             </ul>
         </div>
         <!-- Newsletter area -->
         <div class="footer3">
             <h4>Newsletter</h4>
             <p>Signup to our newsletter for the latest news</p>
-            <form action="index.html" method="post">
+            <form name="newsLetter" action="footer.php" method="post">
                 <input type="email" name="email"  placeholder="Email address"  maxlength="32" size="14">
-                <button type="submit">SUBSCRIBE</button>
+                <!-- Disabled for validation testing for now -->
+              <!--  <button type="submit" onsubmit="return validateNewsletter()">SUBSCRIBE</button> -->
+                <button type="button" onclick="return validateNewsletter()">SUBSCRIBE</button>
             </form>
-  
+
         </div>
-		
+
 		<div class="footer4">
             <h4>Account</h4>
-			<?php 
+			<?php
 			//if admin logged in
 			if (isset($_SESSION['u_ad'])) {
 				echo '<form action="logout.inc.php" method="POST">
 						<a href="admin.php">  Admin Portal</a>
 						<button type="submit" name="submit">Admin Logout</button>
-						
+
 					  </form>';}
 			//if standard user logged in
 			elseif (isset($_SESSION['u_id'])) {
 				echo '<form action="logout.inc.php" method="POST">
 						<button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Logout</button>
-					  </form>';}		
+					  </form>';}
 		 //if not logged in at all
-			else { 
+			else {
 				echo'<ul>
 						<a href="sign-up.php"><b><u>Sign Up</u></b></br></a>
 						Or Log In Below
 					</ul>
-					<form action="login.inc.php" method="POST">
+					<form action="login.inc.php" method="POST" name="login">
 						<input type="username" name="uid"  placeholder="Username"  maxlength="32" size="14">
 						<input type="password" name="pwd"  placeholder="Password"  maxlength="32" size="14">
-						<button type="submit" name ="submit">LOG IN</button>
-					</form>';}	
-			?>	
+            <button type="button" name ="button" onclick="return validateLogin()">LOG IN</button>
+            <!--- Disabled for testing at the moment -->
+            <!---  <button type="submit" name ="submit">LOG IN</button> -->
+					</form>';}
+			?>
         </div>
-		
+
 		<div class="footerCopyright">
             <p>© 2018 Mellow Me Out</p>
         </div>
-</footer> 
+</footer>
--- mellowmeoutFUNCTIONALITY/sign-up.php
+++ mellowmeoutFUNCTIONALITY/sign-up.php
@@ -1,25 +1,76 @@
 <!DOCTYPE html>
 <html>
+<script type="text/javascript">
+
+function validate()
+{
+  var x = document.forms["signUp"]["first"].value;
+  var y = document.forms["signUp"]["last"].value;
+  var z = document.forms["signUp"]["email"].value;
+  var a = document.forms["signUp"]["uid"].value;
+  var b = document.forms["signUp"]["pwd"].value;
+
+  if(x == "")
+  {
+    alert("A first name is required.");
+    return false;
+  }
+
+  if(y == "")
+  {
+    alert("A last name is required.");
+    return false;
+  }
+
+  if(z == "")
+  {
+    alert("An email address is required.");
+    return false;
+  }
+  else if(!z.includes("@") || !z.includes("."))
+  {
+    alert("The email is not in the proper form.");
+    return false;
+  }
+
+  if(a == "")
+  {
+    alert("A username is required.");
+    return false;
+  }
+
+  if(b == "")
+  {
+    alert("A password is required.")
+    return false;
+  }
+
+
+}
+
+
+
+</script>
 <head>
     <title>Create your account</title> <!-- Web Tab Title -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Enabling zoom on mobile devices -->
-    
+
     <meta name="keywords" content="spas, beauty, massages, eyelashes, melbourne">
-    
+
     <meta name="description" content="Relaxing Spas. Book now today!">
-    
+
     <link rel="stylesheet" type="text/css" href="contact-us.css"> <!-- Link to external CSS file -->
     <script type="text/javascript" src="slideshow.js"></script>
 </head>
        <style>
             /* dynamic styling of footer across all pages */
             <?php include 'footer.css'; ?>
-        </style>   
+        </style>
 <body onload="ChangeDots">
     <div class="wrapper">
 <?php include 'header.php';?>
 <?php include 'nav.php';?>
-    
+
 
     <!-- MAIN CONTENT -->
     <div class= "content">
@@ -29,7 +80,7 @@
           <form class="needs-validation" action="signup.inc.php" method="POST">
         </form>
 <div class = "formBody">
-    <form>
+    <form name="signUp">
         <label for="fname">First Name:</label>
          <br>
         <input type="text" class="form-control" name="first" placeholder="Jane" value="" required maxlength="24">
@@ -45,7 +96,7 @@
         <input type="email" class="form-control" name="email" placeholder="you@example.com" maxlength="64">
      <br>
      <br>
-<label for="subject">Username:</label>     
+<label for="subject">Username:</label>
  <br>
      <input type="text" class="form-control" name="uid" placeholder="Enter your username here" required maxlength="24"> <br>
      <br>
@@ -53,17 +104,17 @@
          <br>
         <input type="password" class="form-control" name="pwd" placeholder="Enter your password here" required maxlength="24">
         <br>
-        <input type="submit" value="Create Account" name="submit">
+        <input type="submit" value="Create Account" name="submit" onclick="return validate()">
         <p>By Submitting, you  are agreeing on terms and services and have read the privacy policy.</p>
           </form>
   </div>
         <br><br><br>
 </div>
-       
+
     <!-- MAIN CONTENT END -->
     <!-- FOOTER CONTENT -->
         <?php include 'footer.php';?>
 
- </div>   
+ </div>
 </body>
 </html>

08601ac04c9aaabaf9caaca8029fc1a356aba56b
JiaruiShi <38801546+JiaruiShi@users.noreply.github.com>
2018-05-30 19:18:44 +1000

    Add files via upload

--- mellowmeoutFUNCTIONALITY/blog.php
+++ mellowmeoutFUNCTIONALITY/blog.php
@@ -2,6 +2,44 @@
 <html>
 <head>
     <title>Blog</title> <!-- Web Tab Title -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="carrot Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<script src="js/modernizr.custom.js"></script>
+	<script src="js/snap.svg-min.js"></script>
+				<!--light-box-files -->
+	<script src="js/jquery.chocolat.js"></script>			
+			<script type="text/javascript">
+			$(function() {
+				$('.gallery a').Chocolat();
+			});
+			</script>
+			<!--light-box-files -->
+			<script src="js/responsiveslides.min.js"></script>
+	<script>
+    $(function () {
+      $("#slider").responsiveSlides({
+      	auto: true,
+      	nav: true,
+      	speed: 500,
+        namespace: "callbacks",
+        pager: true,
+      });
+    });	
+	</script>
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+		</script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Enabling zoom on mobile devices -->
     
     <meta name="keywords" content="spas, beauty, massages, eyelashes, melbourne">
@@ -10,9 +48,6 @@
     
     <link rel="stylesheet" type="text/css" href="blog.css"> <!-- Link to external CSS file -->
     <script type="text/javascript" src="slideshow.js"></script>
-	<style>
-	
-	</style>
 </head>
     
 <body onload="ChangeDots">
@@ -25,28 +60,29 @@
         <?php include('nav.php')?>
     
 	<!-- MAIN CONTENT -->
+	
+		
+		
     <div class= "content"> 
        <div id="left">
 		<h2 style="text-align:center">Mellow Me Out Blog</h2>
 		<h2 style="text-align:left">First blog</h2>
-			<p class="categories">May, 2nd, 2018 | <a href>Dairy</a> | <a href>14 Comment</a></p><hr>
+			<p class="categories">May, 1st, 2018 | <a href>Dairy</a> | Tag: <a href>Students</a> | <a href>14 Comment</a></p><hr>
 			<p class= "contents">This is your first article. 
 			Click the edit link to modify or delete the article, 
 			or start writing a new article. 
 			If you wish, you can tell the reader through this article why you created this blog and what you intend to do with it. 
 			If you need help, you are welcome to support BBS.
 			</p>
-		<p style="text-align:right;margin-right:10px">Tagged: <a href>Students</a></p><hr>
 		
 		<h2 style="text-align:left">Second blog</h2>
-			<p class="categories">May, 2nd, 2018 | <a href>Dairy</a> | <a href>14 Comment</a></p><hr>
+			<p class="categories">May, 30, 2018 | <a href>Dairy</a> | Tag: <a href>Business</a> | <a href>14 Comment</a></p><hr>
 			<p class= "contents">This is your first article. 
 			Click the edit link to modify or delete the article, 
 			or start writing a new article. 
 			If you wish, you can tell the reader through this article why you created this blog and what you intend to do with it. 
 			If you need help, you are welcome to support BBS.
 			</p>
-		<p style="text-align:right;margin-right:10px">Tagged: <a href>Business</a></p><hr>
 	   </div>
 	   <div id="right">
 		<h1 style="text-align:center">Blog Bar</h1><hr>

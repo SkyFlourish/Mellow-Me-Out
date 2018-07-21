1bbbecb26259272c43139268050a72617c74d4b0
ZacRoss <37720721+ZacRoss@users.noreply.github.com>
2018-07-21 14:00:07 +1000

    as before

--- input-validation.inc.php
+++ input-validation.inc.php
@@ -1,20 +1,28 @@
 <?php
 
-  function isInputEmpty($input) {
+  function validateInputNotNull($input, bool) {
     // Null is not data type specific
-    if ($input == null) {
-      // Input is null, code behaviour is specified on the
-      // section calling the function.
+    if ($input != null) {
+      // Input is not null, code behaviour is not specified
+      // as it is assumed that the input is present (but perhaps not valid).
       return true;
     }
     else {
-      // Input is not null, code behaviour is not specified
-      // as it is assumed that the input is present (but perhaps not valid).
+      // Input is null, code behaviour is specified on the
+      // section calling the function.
       return false;
     }
   }
 
-  function validateEmailAddressFormat($input) {
+  function validateEmailAddressInputFormat($input) {
     // Called after determining that the input is not empty
-    
+    // Uses a native php function to evaluate the validity of the email address.
+    if (filter_var($input, FILTER_VALIDATE_EMAIL)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
+
+?>

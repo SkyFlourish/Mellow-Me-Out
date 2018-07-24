34dcd18e5d8b35a2116e986379730d3b7c01e211
Connor Vernon <37699282+conjamver@users.noreply.github.com>
2018-07-24 22:17:17 +1000

    CONTACT US PAGE FRONT END VALIDATION - 60% DONE

--- mellowMeOut_sprint3/contact-us.php
+++ mellowMeOut_sprint3/contact-us.php
@@ -29,17 +29,103 @@
         <script type="text/javascript">
         $(document).ready(function() { //run when page loads
             nav_Counter=-1;
-
-          function validateForm() {
-            var x = document.forms["myForm"]["fname"].value;
-                if( document.myForm.fname.value == "" )
-             {
-                alert( "Please provide your name!" );
-                document.myForm.fname.focus() ;
-                return false;
-             }
-            }
+            var formErrorA = false;
+            var formErrorB = false;
+            var formErrorC = false;
+            var formErrorD = false;
+            var letters = /^[A-Za-z]+$/;
+            var numbers = /^[0-9]+$/;
+            var letterNumber = /^[0-9a-zA-Z]+$/;
+            
+            //CODE FOR TEXT AREA COUNTER
+            var text_max = 300; //max character
+            $('#textarea_feedback').html(text_max + ' characters remaining');
+            $('#textarea').keyup(function() { //execute by keys
+            var text_length = $('#textarea').val().length; //get the length
+            var text_remaining = text_max - text_length; //get the new value
+            
+            if(text_remaining==0){
+                 $("#textarea_feedback").css("color","red");
+                        }
+                        else if(text_remaining>0 && text_remaining<50 ){
+                            $("#textarea_feedback").css("color","#f39c12");
+                        }
+                        else
+                        {
+                            $("#textarea_feedback").css("color","#3498db");
+                        }
+                
+            $('#textarea_feedback').html(text_remaining + ' characters remaining');
+            });
+            
         });
+            
+       
+ 
+
+        $('#textarea').keyup(function() { //execute by keys
+            var text_length = $('#textarea').val().length; //get the length
+            var text_remaining = text_max - text_length; //get the new value
+             $('#textarea_feedback').html(text_remaining + ' characters remaining');
+            
+            });
+            
+            function validateForm() {
+                //CHECK FIRST NAME
+            if( document.myForm.fname.value == "" || document.myForm.fname.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/))
+             {
+                document.myForm.fname.focus() ;
+                $(".alert-style").eq(0).css("display","block");
+                formErrorA = true;
+                
+                
+             }else{
+                $(".alert-style").eq(0).css("display","none");
+                 formErrorA = false;
+             }
+                //CHECK LAST NAME
+            if( document.myForm.lname.value == ""|| document.myForm.lname.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/))
+             {
+                 document.myForm.fname.focus() ;
+                $(".alert-style").eq(1).css("display","block");
+                 formErrorB = true;
+                
+             }else{
+                 $(".alert-style").eq(1).css("display","none");
+                 formErrorB = false;
+             }
+                //CHECK EMAIL
+            if( document.myForm.mail.value == "" )
+             {
+                 document.myForm.mail.focus() ;
+                $(".alert-style").eq(2).css("display","block");
+                 formErrorC = true;
+                
+             } else{
+                 $(".alert-style").eq(2).css("display","none"); 
+                 formErrorC = false;
+             }  
+                //CHECK SUBJECT
+            if( document.myForm.subject.value == "" || document.myForm.subject.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/) )
+             {
+                 document.myForm.subject.focus() ;
+                $(".alert-style").eq(3).css("display","block");
+                 formErrorD = true;
+               
+             }else{
+                 $(".alert-style").eq(3).css("display","none");
+                 formErrorD = false;
+             }
+                //CHECK MESSAGE
+                if(formErrorA ==false && formErrorB ==false && formErrorC ==false && formErrorD ==false){
+                    return true;
+                }else{
+                    return false;
+                    alert("errors");
+                }
+            }
+            
+    
             
         </script>   
     </head>
@@ -74,32 +160,39 @@
             <div class="form-group" >
                 <label for="firstName">First Name: </label>
                 <input name="fname" type="text" class="form-control" id="firstName" maxlength="24" placeholder="Enter first name here">
+                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i><strong>ERROR:</strong> First name cannot be blank and must contain only numbers or letters</div>
             </div>
             <div class="form-group">
                 <label for="lastName">Last Name: </label>
-                <input type="text" class="form-control" id="lastName" maxlength="24" placeholder="Enter last name here">
+                <input name="lname" type="text" class="form-control" id="lastName" maxlength="24" placeholder="Enter last name here">
+                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i><strong>ERROR:</strong> Last name cannot be blank and must contain only numbers or letters</div>
             </div>
             <div class="form-group">
                 <label for="mail">Email: </label>
-                <input type="email" class="form-control" id="mail" maxlength="48" placeholder="janedoe@example.com">
+                <input  name="mail" type="email" class="form-control" id="mail" maxlength="48" placeholder="janedoe@example.com">
+                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i>ERROR: Not a valid email address</div>
             </div>
             <div class="form-group">
                 <label for="subject">Subject: </label>
-                <input type="text" class="form-control" id="subject" maxlength="64" placeholder="Subject of the message">
+                <input name="subject" type="text" class="form-control" id="subject" maxlength="64" placeholder="Subject of the message">
+                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i>ERROR: This field is required</div>
             </div>
             <div class="form-group">
+                
                 <label for="message">Message:</label>
-                <textarea class="form-control" rows="5" id="message" maxlength="300" placeholder="Enter your message here"></textarea>
+                <textarea name="message" class="form-control" rows="5" id="textarea" maxlength="300" placeholder="Enter your message here"></textarea>
+                <div class="alert alert-danger alert-style">ERROR: This field is required</div>
             </div>
+            <div id="textarea_feedback">try</div>
             
             <input type="submit" value="Submit" class="btn btn-primary">
+            
         </form>
         </div>
           <div class="col-sm-3">
         
         </div>
     </div>
-    
 
 
 <!-- FOOTER CODE-->

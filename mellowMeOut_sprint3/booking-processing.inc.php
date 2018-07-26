2051f3f9c2f34a342abee4f69937fdab0d347f0a
ZacRoss <37720721+ZacRoss@users.noreply.github.com>
2018-07-26 21:54:31 +1000

    additions

--- mellowMeOut_sprint3/booking-processing.inc.php
+++ mellowMeOut_sprint3/booking-processing.inc.php
@@ -3,7 +3,13 @@
 require 'input-validation.inc.php';
 require 'input-cleanup.inc.php';
 
-$formComplete = true;
+$formComplete = false;
+$fullNameMissing = true;
+$phoneMissing = true;
+$selectBookingMissing = true;
+$serviceMissing = true;
+$emailMissing = true;
+$emailConfirmMissing = true;
 
 // If form is submitted, then the below code will run.
 // Code checks all the form data for any discrepencies and
@@ -16,6 +22,7 @@ if (isset($_POST["submit"])) {
         $fullName = $_POST["fullName"];
         if (validateInputNotNull($fullName) == true) {
             $fullName = stripTagsFromInput($fullName);
+            $fullNameValid = true;
         }
     }
     else {
@@ -26,6 +33,7 @@ if (isset($_POST["submit"])) {
         $phone = $_POST["phone"];
         if (validateInputNotNull($phone) == true) {
             $phone = stripTagsFromInput($phone);
+            $phoneValid = true;
         }
     }
     else {
@@ -36,6 +44,7 @@ if (isset($_POST["submit"])) {
         $selectBooking = $_POST["select-booking"];
         if (validateInputNotNull($selectBooking) == true) {
             $selectBooking = stripTagsFromInput($selectBooking);
+            $selectBookingValid = true;
         }
     }
     else {
@@ -43,29 +52,46 @@ if (isset($_POST["submit"])) {
         $selectBookingMissing = true;
     }
     if (isset($_POST["service"])) {
-        $phone = $_POST["service"];
+        $service = $_POST["service"];
         if (validateInputNotNull($service) == true) {
             $service = stripTagsFromInput($service);
+            $serviceValid = true;
         }
     }
     else {
         $formComplete = false;
         $serviceMissing = true;
     }
-    if (isset($_POST["phone"])) {
-        $confirmEmail = $_POST["phone"];
-        if (validateInputNotNull($confirmEmail) == true) {
+    if (isset($_POST["email"])) {
+        $confirmEmail = $_POST["email"];
+        if (validateInputNotNull($email) == true) {
             if (validateInputNotNull($email) == true) {
                 $email = stripTagsFromInput($email);
-            }
-            else {
-              $formComplete = false;
-              $email = true;
+                $emailValid = true;
             }
         }
     }
     else {
         $formComplete = false;
-        $confirmEmail = true;
+        $emailMissing = true;
+    }
+    if (isset($_POST["confirm-email"])) {
+        $confirmEmail = $_POST["confirm-email"];
+        if (validateInputNotNull($confirmEmail) == true) {
+            if (validateEmailAddressInputFormat($confirmEmail) == true) {
+                $email = stripTagsFromInput($confirmEmail);
+                $confirmEmailValid = true;
+            }
+        }
+    }
+    else {
+        $formComplete = false;
+        $emailConfirmMissing = true;
+    }
+}
+
+if ($fullNameValid == true && $phoneValid == true && $selectBookingValid == true && $serviceValid == true && $emailValid == true && $emailConfirmValid == true && $formComplete == true) {
+    if ($email == $confirmEmailValid) {
+        // Do things here
     }
 }

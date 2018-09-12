c9d66b5af6461cd824534584efb43ebf2bfe4144
Ahmed044 <30297218+Ahmed044@users.noreply.github.com>
2018-09-13 05:10:44 +1000

    Error message formatting updated.
    I have updated the javascript and css for better error-formatting. The errors now show X or tick, dynamically display as user leaves field. There is still overall form validation at the end to make sure the form cannot be submitted with pending issues.

--- mellowMeOut_sprint3/contact-us.php
+++ mellowMeOut_sprint3/contact-us.php
@@ -8,8 +8,8 @@
         <!--Keywords for search-->
         <meta name="keywords" content="Massage, eyelash extensions, beauty, melbourne, mellowMeOut, spas, Brow Tint, Aromatherapy Massage, Lymphatic massage, Indian head massage, Body Mud wrap, Body exfoliate, Eyelash Tint, Combat Stress, Revitalise ">
         <!--Description of page-->
-        <meta name="description" content="Mellow Me Out wants to be your primary choice for all your beauty and relaxation needs. We have truly soothing and calming pampering experiences available for every client. It’s an ideal place to gather with friends, reconnect with loved ones or turn to as your destination for self-renewal and stress relief."> 
-    
+        <meta name="description" content="Mellow Me Out wants to be your primary choice for all your beauty and relaxation needs. We have truly soothing and calming pampering experiences available for every client. It’s an ideal place to gather with friends, reconnect with loved ones or turn to as your destination for self-renewal and stress relief.">
+
         <meta name="viewport" content="width=device-width, initial-scale=1"> <!--Mobile view-->
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
@@ -25,116 +25,261 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
         <!-- FONT AWESOME ICONS -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-                 
-        <script type="text/javascript">
-        $(document).ready(function() { //run when page loads
-            nav_Counter=-1;
-            var formErrorA = false;
-            var formErrorB = false;
-            var formErrorC = false;
-            var formErrorD = false;
-            var letters = /^[A-Za-z]+$/;
-            var numbers = /^[0-9]+$/;
-            var letterNumber = /^[0-9a-zA-Z]+$/;
-            
-            //CODE FOR TEXT AREA COUNTER
-            var text_max = 300; //max character
-            $('#textarea_feedback').html(text_max + ' characters remaining');
-            $('#textarea').keyup(function() { //execute by keys
-            var text_length = $('#textarea').val().length; //get the length
-            var text_remaining = text_max - text_length; //get the new value
-            
-            if(text_remaining==0){
-                 $("#textarea_feedback").css("color","red");
-                        }
-                        else if(text_remaining>0 && text_remaining<50 ){
-                            $("#textarea_feedback").css("color","#f39c12");
-                        }
-                        else
-                        {
-                            $("#textarea_feedback").css("color","#3498db");
-                        }
-                
-            $('#textarea_feedback').html(text_remaining + ' characters remaining');
-            });
-            
-        });
-            
-       
- 
 
-        $('#textarea').keyup(function() { //execute by keys
-            var text_length = $('#textarea').val().length; //get the length
-            var text_remaining = text_max - text_length; //get the new value
-             $('#textarea_feedback').html(text_remaining + ' characters remaining');
-            
-            });
-            
-            function validateForm() {
-                //CHECK FIRST NAME
-            if( document.myForm.fname.value == "" || document.myForm.fname.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/))
-             {
-                document.myForm.fname.focus() ;
-                $(".alert-style").eq(0).css("display","block");
-                formErrorA = true;
-                
-                
-             }else{
-                $(".alert-style").eq(0).css("display","none");
-                 formErrorA = false;
-             }
-                //CHECK LAST NAME
-            if( document.myForm.lname.value == ""|| document.myForm.lname.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/))
-             {
-                 document.myForm.fname.focus() ;
-                $(".alert-style").eq(1).css("display","block");
-                 formErrorB = true;
-                
-             }else{
-                 $(".alert-style").eq(1).css("display","none");
-                 formErrorB = false;
-             }
-                //CHECK EMAIL
-            if( document.myForm.mail.value == "" )
-             {
-                 document.myForm.mail.focus() ;
-                $(".alert-style").eq(2).css("display","block");
-                 formErrorC = true;
-                
-             } else{
-                 $(".alert-style").eq(2).css("display","none"); 
-                 formErrorC = false;
-             }  
-                //CHECK SUBJECT
-            if( document.myForm.subject.value == "" || document.myForm.subject.value.match(/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/) )
-             {
-                 document.myForm.subject.focus() ;
-                $(".alert-style").eq(3).css("display","block");
-                 formErrorD = true;
-               
-             }else{
-                 $(".alert-style").eq(3).css("display","none");
-                 formErrorD = false;
-             }
-                //CHECK MESSAGE
-                if(formErrorA ==false && formErrorB ==false && formErrorC ==false && formErrorD ==false){
-                    return true;
-                }else{
-                    return false;
-                    alert("errors");
-                }
-            }
-            
-    
-            
-        </script>   
+		<script>
+		// overall check when submitting the form
+		function validate_form(frm)
+		{
+			var fname, lname, mail, subject, message;
+			fname = document.getElementById('fname').value;
+			lname = document.getElementById('lname').value;
+			message = document.getElementById('message').value;
+			subject = document.getElementById('subject').value;
+			mail = document.getElementById('mail').value;
+
+			var alpha_pattern = /^[a-zA-Z\s]+$/;
+			var email_pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+			if(fname=="")
+			{
+				alert('First name is mandatory');
+				document.getElementById('fname').focus();
+				return false;
+			}
+			else if(alpha_pattern.test(fname)==false)
+			{
+				alert('The first name must contain alphabets and spaces only');
+				document.getElementById('fname').focus();
+				return false;
+			}
+			else if(lname=="")
+			{
+				alert('Last name is mandatory');
+				document.getElementById('lname').focus();
+				return false;
+			}
+			else if(alpha_pattern.test(lname)==false)
+			{
+				alert('The last name must contain alphabets and spaces only');
+				document.getElementById('lname').focus();
+				return false;
+			}
+			else if(mail=="")
+			{
+				alert('E-Email name is mandatory');
+				document.getElementById('mail').focus();
+				return false;
+			}
+			else if(email_pattern.test(mail)==false)
+			{
+				alert('The email is not in the valid format');
+				document.getElementById('mail').focus();
+				return false;
+			}
+			else if(subject=="")
+			{
+				alert('Subject name is mandatory');
+				document.getElementById('subject').focus();
+				return false;
+			}
+			else if(message=="")
+			{
+				alert('Message is mandatory');
+				document.getElementById('subject').focus();
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+
+		// dynamic error messages
+		$(document).ready(function(){
+
+		  $('#fname').focusout(function(){
+			fname_validate();
+		  });
+
+		  $('#lname').focusout(function(){
+			lname_validate();
+		  });
+
+		  $('#mail').focusout(function(){
+			mail_validate();
+		  });
+
+		  $('#subject').focusout(function(){
+			subject_validate();
+		  });
+
+		  $('#message').focusout(function(){
+			message_validate();
+		  });
+
+		  function fname_validate() {
+			var pattern = /^[a-zA-Z\s]+$/;
+			var fname = $('#fname').val();
+
+			// check that firstname only contains alphabets
+			if(fname !== '') {
+			  if(pattern.test(fname))
+			  {
+				  $('#lblFirstName').css('color','green');
+				  $('#fname').css('border','2px solid green');
+				  $('#success_fname').css('display','inline-block');
+				  $('#error_fname').css('display','none');
+				  $('#warning_fname').css('display','none');
+				  $('#span1_fname').css('display','none');
+				  $('#span2_fname').css('display','none');
+			  }
+			  else
+			  {
+				$('#lblFirstName').css('color','red');
+				$('#fname').css('border','2px solid red');
+				$('#success_fname').css('display','none');
+				$('#error_fname').css('display','inline-block');
+				$('#warning_fname').css('display','none');
+				$('#span1_fname').css('display','inline-block');
+				$('#span2_fname').css('display','none');
+			  }
+			}
+			else { // field is blank
+				$('#lblFirstName').css('color','red');
+				$('#fname').css('border','2px solid red');
+				$('#success_fname').css('display','none');
+				$('#error_fname').css('display','none');
+				$('#warning_fname').css('display','inline-block');
+				$('#span1_fname').css('display','none');
+				$('#span2_fname').css('display','inline-block');
+			}
+		  }
+
+		  function lname_validate() {
+			var pattern = /^[a-zA-Z\s]+$/;
+			var lname = $('#lname').val();
+
+			// check that lastname only contains alphabets
+			if(lname !== '') {
+			  if(pattern.test(lname))
+			  {
+				  $('#lblLastName').css('color','green');
+				  $('#lname').css('border','2px solid green');
+				  $('#success_lname').css('display','inline-block');
+				  $('#error_lname').css('display','none');
+				  $('#warning_lname').css('display','none');
+				  $('#span1_lname').css('display','none');
+				  $('#span2_lname').css('display','none');
+			  }
+			  else
+			  {
+				$('#lblLastName').css('color','red');
+				$('#lname').css('border','2px solid red');
+				$('#success_lname').css('display','none');
+				$('#error_lname').css('display','inline-block');
+				$('#warning_lname').css('display','none');
+				$('#span1_lname').css('display','inline-block');
+				$('#span2_lname').css('display','none');
+			  }
+			}
+			else { // field is blank
+				$('#lblLastName').css('color','red');
+				$('#lname').css('border','2px solid red');
+				$('#success_lname').css('display','none');
+				$('#error_lname').css('display','none');
+				$('#warning_lname').css('display','inline-block');
+				$('#span1_lname').css('display','none');
+				$('#span2_lname').css('display','inline-block');
+			}
+		  }
+
+
+		  function mail_validate() {
+			var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
+			var mail = $('#mail').val();
+
+			// check that email only contains alphabets
+			if(mail !== '') {
+			  if(pattern.test(mail))
+			  {
+				  $('#lblEmail').css('color','green');
+				  $('#mail').css('border','2px solid green');
+				  $('#success_mail').css('display','inline-block');
+				  $('#error_mail').css('display','none');
+				  $('#warning_mail').css('display','none');
+				  $('#span1_mail').css('display','none');
+				  $('#span2_mail').css('display','none');
+			  }
+			  else
+			  {
+				$('#lblEmail').css('color','red');
+				$('#mail').css('border','2px solid red');
+				$('#success_mail').css('display','none');
+				$('#error_mail').css('display','inline-block');
+				$('#warning_mail').css('display','none');
+				$('#span1_mail').css('display','inline-block');
+				$('#span2_mail').css('display','none');
+			  }
+			}
+			else { // field is blank
+				$('#lblEmail').css('color','red');
+				$('#mail').css('border','2px solid red');
+				$('#success_mail').css('display','none');
+				$('#error_mail').css('display','none');
+				$('#warning_mail').css('display','inline-block');
+				$('#span1_mail').css('display','none');
+				$('#span2_mail').css('display','inline-block');
+			}
+		  }
+
+
+		  function subject_validate() {
+			var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+			var subject = $('#subject').val();
+
+			// check that subject is mandatory
+			if(subject != '') {
+
+			}
+			else { // field is blank
+				$('#lblSubject').css('color','red');
+				$('#subject').css('border','2px solid red');
+				$('#success_subject').css('display','none');
+				$('#error_subject').css('display','none');
+				$('#warning_subject').css('display','inline-block');
+				$('#span1_subject').css('display','none');
+				$('#span2_subject').css('display','inline-block');
+			}
+		  }
+
+		function message_validate() {
+			var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+			var message = $('#message').val();
+
+			// check that message is mandatory
+			if(message != '') {
+
+			}
+			else { // field is blank
+				$('#lblMessage').css('color','red');
+				$('#message').css('border','2px solid red');
+				$('#success_message').css('display','none');
+				$('#error_message').css('display','none');
+				$('#warning_message').css('display','inline-block');
+				$('#span1_message').css('display','none');
+				$('#span2_message').css('display','inline-block');
+		  }
+		 }
+
+		});
+	  </script>
     </head>
-<!-- Style Sheet for Nav and Footer-->  
+<!-- Style Sheet for Nav and Footer-->
 <style>
     <?php include 'nav_footer.css';?>
 </style>
-    
- <!-- ===CODE START=== -->    
+
+ <!-- ===CODE START=== -->
 <body>
  <!-- NAVIGATION BAR CODE -->
 <?php include'nav.php';?>
@@ -143,13 +288,13 @@
 <div class="container">
     <div class="row" >
         <div class="col-md-3">
-        
+
         </div>
         <div class="col-md-6">
-        <form name="myForm"  method="post" action="form.php" id="contact-style" onsubmit="return validateForm();" >
+        <form name="myForm"  method="post" action="form.php" id="contact-style" onsubmit="return validate_form(this)">
             <h1 class="text-center">Contact Us</h1>
              <!-- CONTACT INFOMATION CODE -->
-            
+
             <ul class="list-unstyled">
                 <li> <strong>Address:</strong> 123 dove road Boronia, VIC, AUSTRALIA </li>
                 <li> <strong>Phone:</strong>  (03)98278685 </li>
@@ -158,43 +303,59 @@
             <hr>
             <p>Have some questions not covered by our FAQ? Send it from here and we'll be sure to respond as quickly as we can. Here at MellowMeOut, your feedback helps improve our services so that everyone is satisfied.</p>
            <!-- FORM SECTION -->
+            <div class="form-group">
+			  <label id="lblFirstName">First Name</label><br />
+			  <i class="fa fa-check" id="success_fname"></i>
+			  <i class="fa fa-times" id="error_fname"></i>
+			  <i class="fa fa-exclamation" id="warning_fname"></i>
+			  <span class="span1" id="span1_fname">The first name can only contain alphabets</span>
+			  <span class="span2" id="span2_fname">The first name is mandatory</span>
+			  <input type="text" id="fname" name="fname" placeholder="First name here"><br />
+			</div>
             <div class="form-group" >
-                <label for="firstName">First Name: </label>
-                <input name="fname" type="text" class="form-control" id="firstName" maxlength="24" placeholder="Enter first name here">
-                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i><strong>ERROR:</strong> First name cannot be blank and must contain only numbers or letters</div>
-            </div>
+			  <label id="lblLastName">Last Name</label><br />
+			  <i class="fa fa-check" id="success_lname"></i>
+			  <i class="fa fa-times" id="error_lname"></i>
+			  <i class="fa fa-exclamation" id="warning_lname"></i>
+			  <span class="span1" id="span1_lname">The last name can only contain alphabets</span>
+			  <span class="span2" id="span2_lname">The last name is mandatory</span>
+			  <input type="text" id="lname" name="lname" placeholder="Last name here"><br />
+			</div>
             <div class="form-group">
-                <label for="lastName">Last Name: </label>
-                <input name="lname" type="text" class="form-control" id="lastName" maxlength="24" placeholder="Enter last name here">
-                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i><strong>ERROR:</strong> Last name cannot be blank and must contain only numbers or letters</div>
-            </div>
+			  <label id="lblEmail">Email</label><br />
+			  <i class="fa fa-check" id="success_mail"></i>
+			  <i class="fa fa-times" id="error_mail"></i>
+			  <i class="fa fa-exclamation" id="warning_mail"></i>
+			  <span class="span1" id="span1_mail">The email is not in the valid format</span>
+			  <span class="span2" id="span2_mail">The email is mandatory</span>
+			  <input type="text" id="mail" name="mail" placeholder="Email here"><br />
+			</div>
             <div class="form-group">
-                <label for="mail">Email: </label>
-                <input  name="mail" type="email" class="form-control" id="mail" maxlength="48" placeholder="janedoe@example.com">
-                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i>ERROR: Not a valid email address</div>
-            </div>
+			  <label id="lblSubject">Subject</label><br />
+			  <i class="fa fa-check" id="success_subject"></i>
+			  <i class="fa fa-times" id="error_subject"></i>
+			  <i class="fa fa-exclamation" id="warning_subject"></i>
+			  <span class="span2" id="span2_subject">The subject is mandatory</span>
+			  <input type="text" id="subject" name="subject" placeholder="Subject here"><br />
+			</div>
             <div class="form-group">
-                <label for="subject">Subject: </label>
-                <input name="subject" type="text" class="form-control" id="subject" maxlength="64" placeholder="Subject of the message">
-                <div class="alert alert-danger alert-style"><i class="fas fa-exclamation-circle"></i>ERROR: This field is required</div>
+			  <label id="lblMessage">Message</label><br />
+			  <i class="fa fa-check" id="success_message"></i>
+			  <i class="fa fa-times" id="error_message"></i>
+			  <i class="fa fa-exclamation" id="message_message"></i>
+			  <span class="span2" id="span2_message">The message is mandatory</span>
+              <textarea name="message" id="message" id="textarea" maxlength="300" placeholder="Enter your message here"></textarea>
             </div>
-            <div class="form-group">
-                
-                <label for="message">Message:</label>
-                <textarea name="message" class="form-control" rows="5" id="textarea" maxlength="300" placeholder="Enter your message here"></textarea>
-                <div class="alert alert-danger alert-style">ERROR: This field is required</div>
-            </div>
-            <div id="textarea_feedback">try</div>
-            
+
             <input type="submit" value="Submit" class="btn btn-primary">
-            
+
         </form>
         </div>
           <div class="col-md-3">
-        
+
         </div>
     </div>
-</div>    
+</div>
 </article>
 
 <!-- FOOTER CODE-->

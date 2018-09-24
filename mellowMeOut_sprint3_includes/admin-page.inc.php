4f9fe5e3b8a93392b6cb08e91403f98f50bd6983
Lachlanorr95 <37618886+Lachlanorr95@users.noreply.github.com>
2018-09-24 16:12:38 +1000

    Add files via upload

--- mellowMeOut_sprint3_includes/admin-page.inc.php
+++ mellowMeOut_sprint3_includes/admin-page.inc.php
@@ -8,7 +8,7 @@ require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-ser
 require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-services-category.inc.php');
 require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-blog-category.inc.php');
 require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-staff.inc.php');
-//require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-booking.inc.php');
+require ($_SERVER['DOCUMENT_ROOT'].'/mellowMeOut_Sprint3_includes/administer-booking.inc.php');
 
 if (isset($_GET["selection"])) {
     $selection = $_GET["selection"];
@@ -76,14 +76,118 @@ if (isset($_GET["selection"])) {
                 </button>
 				
                 </form>';
-				
 		}
             break;
 
+			
+			
+			
+			
+			
         case '5':
-            // code...
+            //ADD BOOKING
+			echo '
+			<div class="col-md-20 order-md-1"><p></p>Please Select Date of Booking:
+		<form name="submit" action="" id="name" method="POST">
+		<input name = "datepicked" type="date"/><p>
+		<br><button type="submit" name="submit" class="btn btn-primary">
+			Show Available Sessions</button><br>
+		
+		 </form>'; 
+		//if date is submitted and not null show dates 
+		if (isset($_POST['submit']) && $_POST['datepicked'] != "") {
+		include 'db-connect.php';
+		$ar = array("6:30", "6:45", "7:00","7:15", "7:30","7:45", "8:00","8:15", "8:30", "8:45","9:00","9:15", "9:30",
+		"9:45","10:00","10:15", "10:30","10:45", "11:00","11:15", "11:30","11:45", "12:00", "12:15",	
+		"12:30", "12:45","13:00", "13:15","13:30","13:45", "14:00","14:15", "14:30", "14:45","15:00", 
+		"15:15","15:30","15:45", "16:00", "16:15","16:30", "16:45","17:00","17:15", "17:30");
+		echo '   Available sessions for: ' . $_POST["datepicked"] . '   ';
+		echo'<p><div class="col-md-8 order-md-1">
+		<form name="addbooking" action="admin-page.php?action=addBooking" method="POST">
+		<div class="col-md-20 order-md-1">
+		<select name ="add">';
+		for($i=1;$i <= 45; $i++) {
+			$start = $_POST["datepicked"];
+			$str = "$start-$i";
+			
+			$result = mysqli_query($conn, "SELECT user_booking FROM booking WHERE user_booking = '$str'");
+			if(mysqli_num_rows($result) == 0)
+			{echo "<option value='$str'>" .  $ar[($i-1)] . "</option>";	}	
+			else {echo "<option disabled>Booked Out" .  $ar[($i-1)] . "</option>";	}
+		}
+		echo'
+		</select>
+		<p></p><button type="submit" name="addbooking" class="btn btn-primary" >
+			Book Out Selected Session</button>
+		</form>
+		</div>';
+		}
+		echo'</div></div>';
+			
             break;
-
+		case '17':
+            //DELETE BOOKING
+			if (isset($_POST['addbooking']) && $_POST['datepicked'] != "") {
+		include 'db-connect.php';
+		$ar = array("6:30", "6:45", "7:00","7:15", "7:30","7:45", "8:00","8:15", "8:30", "8:45","9:00","9:15", "9:30",
+		"9:45","10:00","10:15", "10:30","10:45", "11:00","11:15", "11:30","11:45", "12:00", "12:15",	
+		"12:30", "12:45","13:00", "13:15","13:30","13:45", "14:00","14:15", "14:30", "14:45","15:00", 
+		"15:15","15:30","15:45", "16:00", "16:15","16:30", "16:45","17:00","17:15", "17:30");
+		echo 'Booked sessions to delete: ' . $_POST["datepicked"] . '   ';
+		echo'<form name="deletebooking" action="delete.inc.php" method="POST">
+		<select name ="deletebooked">';
+		for($i=1;$i <= 45; $i++) {
+			$start = $_POST["datepicked"];
+			$str = "$start-$i";
+			
+			$result = mysqli_query($conn, "SELECT user_booking FROM booking WHERE user_booking = '$str'");
+			if(mysqli_num_rows($result) == 0)
+			{echo "<option disabled>" .  $ar[($i-1)] . "</option>";	}	
+			else {echo "<option value = '$str'>Booked " .  $ar[($i-1)] . "</option>";	}
+		}
+		echo'
+		</select>
+		<br><div class="col-md-20 order-md-1"><p></p><button type="submit" name="deletebooking" class="btn btn-primary">
+			Unbook Selected Session</button></div>
+		</form>';
+			}	
+			
+			  echo '<div class="col-md-20 order-md-1"><p></p>Please Select Date of Booking:
+		<form name="submitdelete" action="" id="name" method="POST">
+		<input name = "datepickeddelete" type="date"/>
+		<p></p><button type="submit" name="submitdelete" class="btn btn-primary">
+			Show Sessions to delete/unbook</button></br></div>
+		
+		 </form>'; 
+		
+		//admin version to delete booking
+		if (isset($_POST['submitdelete']) && $_POST['datepickeddelete'] != "") {
+		include 'db-connect.php';
+		$ar = array("6:30", "6:45", "7:00","7:15", "7:30","7:45", "8:00","8:15", "8:30", "8:45","9:00","9:15", "9:30",
+		"9:45","10:00","10:15", "10:30","10:45", "11:00","11:15", "11:30","11:45", "12:00", "12:15",	
+		"12:30", "12:45","13:00", "13:15","13:30","13:45", "14:00","14:15", "14:30", "14:45","15:00", 
+		"15:15","15:30","15:45", "16:00", "16:15","16:30", "16:45","17:00","17:15", "17:30");
+		echo 'Booked sessions to delete: ' . $_POST["datepickeddelete"] . '   ';
+		echo'<form name="deletebooking" action="admin-page.php?action=deleteBooking" method="POST">
+		<select name ="deletebooked">';
+		for($i=1;$i <= 45; $i++) {
+			$start = $_POST["datepickeddelete"];
+			$str = "$start-$i";
+			
+			$result = mysqli_query($conn, "SELECT user_booking FROM booking WHERE user_booking = '$str'");
+			if(mysqli_num_rows($result) == 0)
+			{echo "<option disabled>" .  $ar[($i-1)] . "</option>";	}	
+			else {echo "<option value = '$str'>Booked " .  $ar[($i-1)] . "</option>";	}
+		}
+		echo'
+		</select>
+		<p></p><button type="submit" name="deletebooking" class="btn btn-primary">
+			Unbook Selected Session</button>
+		</form>';
+			}	
+            break;
+			
+			
         case '6':
             echo '<form action="admin-page.php?action=removeBlog" method="POST">';
 			
@@ -351,6 +455,14 @@ if (isset($_GET["selection"])) {
 if (isset($_GET["action"])) {
     $action = $_GET["action"];
     switch ($action) {
+	case 'addBooking':
+        // code...
+        addBooking();
+        break;
+	case 'deleteBooking':
+        // code...
+        removeBooking();
+        break;
     case 'addBlog':
         // code...
         addBlogEntry();
